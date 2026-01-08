a070fefa6c833be9ead86f2bbd2f498ff6f560d5
Espinaler <nmefano23@gmail.com>
2026-01-08 20:06:00 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/nicolas/taskmanager/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tunombre.taskmanager.repository;
+
+import com.tunombre.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
